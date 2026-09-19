42b05f1789bd4341419ee05310c0242ffdd626fa
Mihir-Rabari <mihirrabari2604@gmail.com>
2026-09-19 18:50:52 +0000

    fix: remove ee dependency from role seeding (CE-native project role entity)

--- packages/server/api/src/app/database/seeds/role-seed.ts
+++ packages/server/api/src/app/database/seeds/role-seed.ts
@@ -1,16 +1,16 @@
 import { RoleType } from '@inboxfm-connect/core-utils'
 import { DefaultProjectRole, rolePermissions } from '@inboxfm-connect/shared'
 import { repoFactory } from '../../core/db/repo-factory'
-import { ProjectRoleEntity } from '../../ee/projects/project-role/project-role.entity'
 import { system } from '../../helper/system/system'
+import { ProjectRoleEntity } from '../../project/project-role/project-role.entity'
 import { DataSeed } from './data-seed'
 
-const projectMemberRoleRepo = repoFactory(ProjectRoleEntity)
+const projectRoleRepo = repoFactory(ProjectRoleEntity)
 
 // DO NOT CHANGE THESE IDS OR SHUFFLE THEM
 const roleIds: Record<DefaultProjectRole, string> = {
     [DefaultProjectRole.ADMIN]: '461ueYHzMykyk5dIL8HzQ',
-    [DefaultProjectRole.EDITOR]: 'sjWe85TwaFYxyhn2AgOha', 
+    [DefaultProjectRole.EDITOR]: 'sjWe85TwaFYxyhn2AgOha',
     [DefaultProjectRole.VIEWER]: 'aJVBSSJ3YqZ7r1laFjM0a',
 }
 
@@ -19,7 +19,7 @@ export const rolesSeed: DataSeed = {
         system.globalLogger().info({ name: 'rolesSeed' }, 'Seeding roles')
         for (const role of Object.values(DefaultProjectRole)) {
             const permissions = rolePermissions[role]
-            await projectMemberRoleRepo().upsert({
+            await projectRoleRepo().upsert({
                 name: role,
                 permissions,
                 type: RoleType.DEFAULT,
